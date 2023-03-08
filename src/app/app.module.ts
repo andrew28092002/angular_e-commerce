1160f4586cf84ef7131430b9838a2cb216dd2b49
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-08 22:04:50 +0500

    added drawer in home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './page/home/home.component';
+import { ProductsHeaderComponent } from './pages/components/products-header/products-header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

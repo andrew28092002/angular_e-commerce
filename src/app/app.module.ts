a0c2bf6dacbd6fc7c490d06269e5474087290e63
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-14 23:06:37 +0500

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './page/home/home.component';
-import { ProductsHeaderComponent } from './pages/components/products-header/products-header.component';
+import { ProductsHeaderComponent } from './page/components/products-header/products-header.component';
+import { FiltersComponent } from './page/components/filters/filters.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  cols: number = 3;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onColumnsCountChange(colsNumber: number): void{
+    this.cols = colsNumber
+  }
 
 }

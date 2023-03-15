12ab6d7a327a735ee9c6834ac6b2b74b452d92cd
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-15 16:48:52 +0500

    fix add-product butto

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 const ROWS_HEIGHT: {[id:number]: number} = {
   1: 400, 3:335, 4: 350
@@ -14,6 +16,8 @@ export class HomeComponent implements OnInit{
   category: string | undefined;
   rowHeight = ROWS_HEIGHT[this.cols]
 
+  constructor(private CartService: CartService){}
+
   ngOnInit(): void {
   }
 
@@ -26,7 +30,13 @@ export class HomeComponent implements OnInit{
     this.category = newCategory
   }
 
-  onAddToCart(){
-    
+  onAddToCart(product: Product){
+    this.CartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    })
   }
 }

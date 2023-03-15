15b84a7e0b7d194d868fa5166db8b9af2ff544e5
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-15 13:44:44 +0500

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './page/components/cart/cart.component';
 import { HomeComponent } from './page/home/home.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
 ];
 

1c6474c5149b4b39efc2fd3bc18fbcadf90a8862
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-15 13:53:54 +0500

    update folders

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  cart: Cart = { items: [] };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
